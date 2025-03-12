6e2c0a13917140777d4e30bcb786d185a4a966d7
VojtechStep <vojtechstepancik@outlook.com>
2025-03-12 09:52:05 +0100

    uncomment Step 3

--- TP.py
+++ TP.py
@@ -7,8 +7,8 @@ print("Welcome to the Git exercise!")
 # print("5 - 2 =", subtract(5, 2))
 
 # Step 3: Random fun
-# import random
-# print("Lucky number:", random.randint(1, 100))
+import random
+print("Lucky number:", random.randint(1, 100))
 
 # Step 4: Final message
 # print("Great job completing the steps!")
